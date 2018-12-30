ec4ca3753e06543df7322b6e5a6921fc934dc239
Yanfang Cui <yanfangcui@Yanfangs-MacBook-Pro.local>
2018-12-30 13:12:26 +0800

    chapter 1

---
+++ taco-cloud-chapter1/src/main/java/tacos/TacoCloudChapter1Application.java
@@ -0,0 +1,14 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudChapter1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudChapter1Application.class, args);
+	}
+
+}
+
