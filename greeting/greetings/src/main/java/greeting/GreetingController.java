c174cb2d3b160fa84a30880afdbcc62433cf7226
Yanfang Cui <yanfangcui@yanfangs-mbp.mynet>
2019-01-07 00:07:57 +0800

    chapter14.4 acutator refresh

---
+++ greeting/greetings/src/main/java/greeting/GreetingController.java
@@ -0,0 +1,19 @@
+package greeting;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	private final GreetingProps props;
+
+	public GreetingController(GreetingProps props) {
+		this.props = props;
+	}
+
+	@GetMapping("/hello")
+	public String message() {
+		return props.getMessage();
+	}
+}
---
+++ greeting/greetings/src/main/java/greeting/GreetingProps.java
@@ -0,0 +1,19 @@
+package greeting;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "greeting")
+@Component
+public class GreetingProps {
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
